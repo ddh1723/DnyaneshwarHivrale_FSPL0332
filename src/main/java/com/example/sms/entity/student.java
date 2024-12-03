a79da408da691eaa9fc174f161539e6be5bab8b0
dnyaneshwarhivrale <ddh1723@gmail.com>
2024-12-03 10:06:04 +0530

    first commit

--- src/main/java/com/example/sms/entity/student.java
+++ src/main/java/com/example/sms/entity/student.java
@@ -16,34 +16,17 @@ import java.time.LocalDateTime;
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private int id;
 
-        @Column(nullable = false, length = 50)
+
+
         private String firstName;
-
-        @Column(length = 50)
         private String middleName;
-
-        @Column(nullable = false, length = 50)
         private String lastName;
-
-        @Column(nullable = false, unique = true, length = 100)
         private String email;
-
-        @Column(nullable = false, unique = true, length = 15)
         private String phoneNumber;
-
-        @Column(nullable = false)
         private boolean isActive;
-
-        @Column(nullable = true, length = 50)
         private String createdBy;
-
-        @Column(nullable = true)
         private LocalDateTime createdDate;
-
-        @Column(nullable = true, length = 50)
         private String updatedBy;
-
-        @Column(nullable = true)
         private LocalDateTime updatedDate;
 
 
